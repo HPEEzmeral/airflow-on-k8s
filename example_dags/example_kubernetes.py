05b96b95cfa17bf19c215e3d28bd27c0a2c76e3f
andriishevliakov <87184322+andriishevliakov@users.noreply.github.com>
2022-08-01 15:46:52 +0300

    Update example_kubernetes.py

--- example_dags/example_kubernetes.py
+++ example_dags/example_kubernetes.py
@@ -48,7 +48,7 @@ with DAG(
     ]
 
     k = KubernetesPodOperator(
-        namespace='kubeflow',
+        namespace='argocd',
         image="ubuntu:16.04",
         cmds=["bash", "-cx"],
         arguments=["echo hello here"],
